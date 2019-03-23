51d0b9f0ddea49ea3c65369005ef8d2ed0e3ab11
Vitalik-D <vitalikdyaduchko@gmail.com>
2019-03-23 12:04:43 +0200

    added css

--- src/components/Game.js
+++ src/components/Game.js
@@ -69,8 +69,8 @@ class Game extends React.Component {
           <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         </div>
         <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+          <div className="title">{status}</div>
+          <ol className="moves">{moves}</ol>
         </div>
       </div>
     );
